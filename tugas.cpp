f2668db03dbf07ff1e1da2475f54e6ffcd3dde32
akmalropip <akmalropip@gmail.com>
2026-04-27 17:10:35 +0700

    membuat class belahketupat dan layang-layang

--- tugas.cpp
+++ tugas.cpp
@@ -3,3 +3,22 @@
 //setiap variable memiliki akses private, bangundatar belah ketupat merupakan function keliling dari layang-layang.
 //semua class hanya dapat diakses menggunakan object.
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class BelahKetupat;
+class LayangLayang
+{
+    private:
+        float d1, d2;
+        float sisi1, sisi2;
+    public:
+        void input();
+        float Luas();
+        float Keliling();
+        void output();
+        
+        // Friend function declaration
+        friend class BelahKetupat;
+};
