2220ed154c8d12a3d0a468a3c8e98d3ab8b4571d
akmalropip <akmalropip@gmail.com>
2026-04-27 15:29:12 +0700

    membuat program utama di pointerSatu

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -7,3 +7,17 @@ class mahasiswa{
         cout<<"No Induk = "<<nim<<endl;
     }
 };
+
+int main (){
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); //member access operator
+
+    mahasiswa &refMhs = mhs; //pointer reference refMhs
+    refMhs.nim = 2; //member access operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer dereference refMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    return 0;
+}
