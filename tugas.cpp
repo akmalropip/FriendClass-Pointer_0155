a2966c8486fc57f59b8b58764b59d50fbb555f40
akmalropip <akmalropip@gmail.com>
2026-04-27 17:09:35 +0700

    menambahkan file tugas

---
+++ tugas.cpp
@@ -0,0 +1,5 @@
+//buatlah sebuah program yang berisi 2 class dimana terdapat class layang-layang, belah ketupat.
+//di setiap class nya berisikan luas dan keliling serta input dan output.
+//setiap variable memiliki akses private, bangundatar belah ketupat merupakan function keliling dari layang-layang.
+//semua class hanya dapat diakses menggunakan object.
+
