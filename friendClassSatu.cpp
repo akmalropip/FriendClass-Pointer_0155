e5bbdbaa693028fdf4fb233ce8a89173d7ff310e
akmalropip <akmalropip@gmail.com>
2026-04-27 15:05:55 +0700

    menambahkan void

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -9,6 +9,20 @@ private:
 string nama;
 
 public:
-void setNama(string Nama);
+void setNama(string pNama);
 friend class siswa;
+};
+
+class siswa
+{
+    private:
+    int id;
+
+    public:
+    void setId(int pId);
+    void displayAll(orang &a);
+
+    public:
+    void setId(int pId);
+    void displayAll(orang &a);
 };
